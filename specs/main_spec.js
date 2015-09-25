60dc16ae97c327f7eb41ea00ad4d897465920b4c
Jon Tore Hafstad <jontore@gmail.com>
2015-09-25 10:43:53 +0200

    Needs to use deep equal

--- specs/main_spec.js
+++ specs/main_spec.js
@@ -4,7 +4,7 @@
   describe('main spec', function () {
     it('4 x 4 ', function () {
       var out = window.MineSweeper(4, 4, ['*...', '....', '.*..', '....']);
-      expect(out).to.be.equal(['*100', '2210', '1*10', '1110']);
+      expect(out).to.be.deep.equal(['*100', '2210', '1*10', '1110']);
     });
   });
 
